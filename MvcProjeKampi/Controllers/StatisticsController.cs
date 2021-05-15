6368e6236e853ab8004d148ab58d70f3bffa9493
caglarhasan <hsncglr07@gmail.com>
2021-05-15 08:23:21 +0300

    Ders 40 - Sertifika Görevi 2/4
    40. derse kadar olan bütün bölümlerin kodları yazılmıştır ve 2. sertifika görevi başarıyla tamamlanmıştır.

---
+++ MvcProjeKampi/Controllers/StatisticsController.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var rslt1 = c.Categories.Count();
+            var rslt2 = c.Headings.Count(x => x.Category.CategoryID == 4011);
+            var rslt3 = c.Writers.Count(x => x.WriterName.Contains("a"));
+            var rslt4 = c.Headings.Max(x => x.Category.CategoryName);
+            var rslt5 = c.Categories.Count(x => x.CategoryStatus == true);
+            var rslt6 = c.Categories.Count(x => x.CategoryStatus == false);
+
+            ViewBag.CategoryCount = rslt1;
+            ViewBag.Heading = rslt2;
+            ViewBag.Writer = rslt3;
+            ViewBag.MaxHeading = rslt4;
+            ViewBag.Status = (rslt5 - rslt6);
+
+            return View();
+        }
+    }
+}
